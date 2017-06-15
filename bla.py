eca4563c8f75e7cd57f1e887d06dddc6e89ab055
johncenanumber1 <facublasco@hotmail.com>
2017-06-15 08:39:18 -0300

    Create bla.py

--- bla.py
+++ bla.py
@@ -23,3 +23,46 @@ while True:
         d=d+1
         if d==100:
             d=1
+            
+    if input == 'Teleport East':
+        mc.player.setPos(x+20, y, z)
+
+    if input == 'Teleport West':
+        mc.player.setPos(x-20, y, z)
+
+    if input == 'Teleport North':
+        mc.player.setPos(x, y, z+20)
+
+    if input == 'Teleport South':
+        mc.player.setPos(x, y, z-20)
+
+    if input == 'Tower':
+        mc.setBlocks(x+11, y+2, z+9, x+22, y+12, z+16, 42)
+        mc.setBlocks(x+12, y+3, z+10, x+21, y+11, z+15, 0)
+        mc.setBlocks(x+10, y+2, z+8, x+22, y+2, z+16, 41)
+        mc.setBlocks(x+9, y+1, z+7, x+23, y+1, z+17, 41)
+        mc.setBlocks(x+8, y+0, z+6, x+24, y+0, z+18, 41)
+        mc.setBlocks(x+7, y-1, z+5, x+25, y-1, z+19, 41)
+        mc.setBlocks(x+6, y-2, z+4, x+26, y-2, z+20, 41)
+        mc.setBlocks(x+5, y-3, z+3, x+27, y-3, z+21, 41)
+        mc.setBlocks(x+4, y-4, z+2, x+28, y-4, z+22, 41)
+        mc.setBlocks(x+16, y+3, z+16, x+17, y+4, z+16, 0)
+        mc.setBlocks(x+16, y+3, z+16, x+17, y+3, z+16, 64)
+        mc.setBlock(x+11, y+13, z+9, 42)
+        mc.setBlock(x+13, y+13, z+9, 42)
+        mc.setBlock(x+15, y+13, z+9, 42)
+        mc.setBlock(x+18, y+13, z+9, 42)
+        mc.setBlock(x+20, y+13, z+9, 42)
+        mc.setBlock(x+22, y+13, z+9, 42)
+        mc.setBlock(x+11, y+13, z+16, 42)
+        mc.setBlock(x+13, y+13, z+16, 42)
+        mc.setBlock(x+15, y+13, z+16, 42)
+        mc.setBlock(x+18, y+13, z+16, 42)
+        mc.setBlock(x+20, y+13, z+16, 42)
+        mc.setBlock(x+22, y+13, z+16, 42)
+        mc.setBlock(x+11, y+13, z+11, 42)
+        mc.setBlock(x+22, y+13, z+11, 42)
+        mc.setBlock(x+11, y+13, z+14, 42)
+        mc.setBlock(x+22, y+13, z+14, 42)
+   if input == 'River':
+        mc.setBlocks(x+11, y+1, z+9, x+22, y+1, z+16, 42)
